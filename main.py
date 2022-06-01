0ef800a4c9abadc4eab8d4e5bd7ec1acb2dc0d90
Yerim Ji <89132581+dpflawl@users.noreply.github.com>
2022-06-01 17:39:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 
 @st.cache(hash_funcs={transformers.models.gpt2.tokenization_gpt2_fast.GPT2TokenizerFast: hash}, suppress_st_warning=True)
 def load_data():    
- tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-medium")
- model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-medium")
+ tokenizer = AutoTokenizer.from_pretrained("beomi/kcbert-base")
+ model = AutoModelWithLMHead.from_pretrained("beomi/kcbert-base")
  return tokenizer, model
  
 tokenizer, model = load_data()
